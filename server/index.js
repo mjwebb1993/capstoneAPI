f0ac3015fce69f4019eca1bc39b050ae623b1c42
mjwebb1993 <mjwebb1993@yahoo.com>
2021-06-22 15:23:21 -0500

    fixed all datalists

--- server/index.js
+++ server/index.js
@@ -23,92 +23,52 @@ const con = MongoClient.connect(
 );
 
 class DataStructure {
-  constructor(name, yearReleased, author, [genre]) {
+  constructor(name, yearReleased, [...genre]) {
     this.name = name;
     this.yearReleased = yearReleased;
-    this.author = author;
     this.genre = genre;
   }
 }
 
-// class Game extends DataStructure {
-//   constructor(name, yearReleased, console, [genre]) {
-//     super(name, yearReleased, [genre]);
-//     this.console = console;
-//   }
-// }
-
-// class Movie extends DataStructure {
-//   constructor(name, yearReleased, distributor, [genre]) {
-//     super(name, yearReleased, [genre]);
-//     this.distributor = distributor;
-//   }
-// }
-class Book extends DataStructure {
-  constructor(name, yearReleased, author, [genre]) {
-    super(name, yearReleased, author, [genre]);
+class Game extends DataStructure {
+  constructor(name, yearReleased, [...genre], console) {
+    super(name, yearReleased, [...genre]);
+    this.console = console;
   }
 }
-// const game1 = new Game("Mass Effect", 2007, "Multi", [
-//   "Action",
-//   "RPG",
-//   "Adventure"
-// ]);
-// const game2 = new Game("Mass Effect 2", 2010, "Multi", [
-//   "Action",
-//   "RPG",
-//   "Adventure"
-// ]);
-// const game3 = new Game("Mass Effect 3", 2012, "Multi", [
-//   "Action",
-//   "RPG",
-//   "Adventure"
-// ]);
-// const game4 = new Game("Super Mario Galaxy 2", 2010, "Wii", [
-//   "Platform",
-//   "Action"
-// ]);
-// const game5 = new Game(
-//   "The Legend of Zelda: Breath of the Wild",
-//   2017,
-//   "Switch",
-//   ["Action", "Adventure", "Open World"]
-// );
 
-// const movie1 = new Movie("Endgame", 2019, "Disney", [
-//   "Action",
-//   "Superhero",
-//   "Adventure"
-// ]);
-// const movie2 = new Movie("Star Wars: The Force Awakens", 2015, "Disney", [
-//   "Science Fiction",
-//   "Action",
-//   "Adventure"
-// ]);
-// const movie3 = new Movie("Avengers: Infinity War", 2018, "Disney", [
-//   "Action",
-//   "Superhero",
-//   "Adventure"
-// ]);
-// const movie4 = new Movie("Jurasic World", 2015, "Universal", [
-//   "Science Fiction",
-//   "Action"
-// ]);
-// const movie5 = new Movie("Furious 7", 2015, "Universal", [
-//   "Action",
-//   "Thriller",
-//   "Cars"
-// ]);
+class Movie extends DataStructure {
+  constructor(name, yearReleased, [...genre], distributor) {
+    super(name, yearReleased, [...genre]);
+    this.distributor = distributor;
+  }
+}
+class Book extends DataStructure {
+  constructor(name, yearReleased, author, [...genre]) {
+    super(name, yearReleased, author, [...genre]);
+  }
+}
+const game1 = new Game("Mass Effect", 2007, ["Action", "RPG", "Adventure"], "Multi");
+const game2 = new Game("Mass Effect 2", 2010, ["Action", "RPG", "Adventure"], "Multi");
+const game3 = new Game("Mass Effect 3", 2012,  ["Action", "RPG", "Adventure"], "Multi");
+const game4 = new Game("Super Mario Galaxy 2", 2010,["Platform", "Action"],  "Wii");
+const game5 = new Game("The Legend of Zelda: Breath of the Wild", 2017, ["Action", "Adventure", "Open World"], "Switch");
 
-const book1 = new Book("The Last Samurai", 2000, "Helen DeWitt", ["Fiction"]);
-const book2 = new Book("The Sellout", 2015, "Paul Beatty", ["Fiction"]);
-const book3 = new Book("The Argonauts", 2015, "Maggie Nelson", ["Fiction"]);
+const movie1 = new Movie("Endgame", 2019, ["Action", "Superhero", "Adventure"], "Disney");
+const movie2 = new Movie("Star Wars: The Force Awakens", 2015, ["Science Fiction", "Action", "Adventure"], "Disney");
+const movie3 = new Movie("Avengers: Infinity War", 2018, ["Action", "Superhero", "Adventure"], "Disney");
+const movie4 = new Movie("Jurasic World", 2015, ["Science Fiction", "Action"], "Universal");
+const movie5 = new Movie("Furious 7", 2015, ["Action", "Thriller", "Cars"], "Universal");
 
-// let gameList = [game1, game2, game3, game4, game5];
-// let movieList = [movie1, movie2, movie3, movie4, movie5];
+const book1 = new Book("The Last Samurai", 2000, ["Fiction"], "Helen DeWitt");
+const book2 = new Book("The Sellout", 2015, ["Fiction"], "Paul Beatty");
+const book3 = new Book("The Argonauts", 2015, ["Fiction"], "Maggie Nelson");
+
+let gameList = [game1, game2, game3, game4, game5];
+let movieList = [movie1, movie2, movie3, movie4, movie5];
 let bookList = [book1, book2, book3];
 
-// let mediaList = [gameList, movieList, bookList];
+let mediaList = [gameList, movieList, bookList];
 
 
 // CORS Middleware
@@ -126,34 +86,34 @@ let bookList = [book1, book2, book3];
 //   next();
 // };
 
-// app.route("/listGames").get((request, response) => {
-//   let listGames = gameList;
+app.route("/listGames").get((request, response) => {
+  let listGames = gameList;
 
-//   response.status(200).json(listGames);
-// });
+  response.status(200).json(listGames);
+});
 
-// app.route("/listMovies").get((request, response) => {
-//   let listMovies = movieList;
+app.route("/listMovies").get((request, response) => {
+  let listMovies = movieList;
 
-//   response.status(200).json(listMovies);
-// });
+  response.status(200).json(listMovies);
+});
 
-// app.route("/listBooks").get((request, response) => {
-//   let listBooks = bookList;
+app.route("/listBooks").get((request, response) => {
+  let listBooks = bookList;
 
-//   response.status(200).json(listBooks);
-// });
+  response.status(200).json(listBooks);
+});
 
-// app.route("/listMedia").get((request, response) => {
-//   let listMedia = mediaList;
+app.route("/listMedia").get((request, response) => {
+  let listMedia = mediaList;
 
-//   response.status(200).json(listMedia);
-// });
+  response.status(200).json(listMedia);
+});
 
 app
   .route("/")
   .get((request, response) => {
-    response.send(bookList);
+    response.send("Hello World!");
   })
   .post((request, response) => {
     response.json(request.body);
